2ed9693f7135387600f2a615f3aae0f6b40f06c3
Kolbi Lee <kolbi5822@gmail.com>
2026-03-02 22:00:32 -0500

    First test of OpenGL and creating a window

--- Learn_OpenGL/main.cpp
+++ Learn_OpenGL/main.cpp
@@ -1,14 +1,34 @@
-//
-//  main.cpp
-//  Learn_OpenGL
-//
-//  Created by Kolbi Lee on 3/2/26.
-//
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    if (!glfwInit()) {
+        std::cout << "Failed to init GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
