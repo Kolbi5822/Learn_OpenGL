c27c485419f2ac95fb061d3b6929d46c89a9904a
Kolbi Lee <kolbi5822@gmail.com>
2026-03-09 19:08:56 -0400

    Working with adding and minipulating different shapes as well as user input.

--- Learn_OpenGL/main.cpp
+++ Learn_OpenGL/main.cpp
@@ -8,17 +8,23 @@
 const char* vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 color;
+    out vec3 vertexColor;
     uniform mat4 transform;
+    uniform mat4 projection;
     void main() {
-        gl_Position = transform * vec4(aPos, 1.0);
+        gl_Position = projection * transform * vec4(aPos, 1.0);
+        vertexColor = color;
     }
 )";
 
 const char* fragmentShaderSource = R"(
     #version 330 core
     out vec4 FragColor;
+    in vec3 vertexColor;
     void main() {
-        FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        FragColor = vec4(vertexColor, 1.0);
+        
     }
 )";
 
@@ -29,7 +35,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Rotating Triangle", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Rotating Triangle", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
@@ -49,10 +55,11 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    float h = sqrt(3.0f) / 2.0f;
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        -0.5f, -h/3.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -h/3.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.0f,  2.0f*h/3.0f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
     unsigned int VAO, VBO;
@@ -61,24 +68,76 @@ int main() {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    
+    float sqr_vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+    
+    unsigned int VAOS, VBOS;
+    glGenVertexArrays(1, &VAOS);
+    glGenBuffers(1, &VBOS);
+    glBindVertexArray(VAOS);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOS);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(sqr_vertices), sqr_vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    float offsetX = 0;
+    float offsetY = 0;
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Create rotation matrix
+        // User Input
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+            offsetY += 0.01f;
+        } else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+            offsetY -= 0.01f;
+        } else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            offsetX -= 0.01f;
+        } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            offsetX += 0.01f;
+        }
+        
+        // Create trangle rotation matrix
         float timeValue = glfwGetTime();
         glm::mat4 transform = glm::mat4(1.0f);
-        transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.0f));
+        transform = glm::translate(transform, glm::vec3(offsetX, offsetY, 0.0f));
         transform = glm::rotate(transform, timeValue, glm::vec3(0.0f, 0.0f, 1.0f));
-
+        
+        //Square transform
+        glm::mat4 transformS = glm::mat4(1.0f);
+        
+        //Projection
+        glm::mat4 projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f);
+        
         // Send it to the shader
-        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        unsigned int transformLocS = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLocS, 1, GL_FALSE, glm::value_ptr(transformS));
+        
+        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
         glUseProgram(shaderProgram);
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
+        glBindVertexArray(VAOS);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
